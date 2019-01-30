a663d949004301ad25f375363af44b1f95a0b025
Sambrana Ivan <sambranaivan@gmail.com>
2019-01-30 08:52:10 -0300

    estilos carnval

--- resources/views/carnaval/results.blade.php
+++ resources/views/carnaval/results.blade.php
@@ -3,27 +3,27 @@
 @section('content')
 
 <div class="container">
-    
-        
-    <div id="chartProcedencia"  style="float:left;height: 300px; width: 50%;"></div>
-    <div id="chartTransporte"  style="float:left;height: 300px; width: 50%;"></div>
-        <div id="chartSexo"  style="float:left;height: 300px; width: 50%;"></div>
-        <div id="chartAlojamiento"  style="float:left;height: 300px; width: 50%;"></div>
-        <div id="chartTipoAlojamiento"  style="float:left;height: 300px; width: 50%;"></div>
-        <div id="chartGastos"  style="float:left;height: 300px; width: 50%;"></div>
-        <div id="chartMotivo"  style="float:left;height: 300px; width: 50%;"></div>
-        <div id="chartInformo"  style="float:left;height: 300px; width: 50%;"></div>
-        <div id="chartViaje"  style="float:left;height: 300px; width: 50%;"></div>
 
 
-    
+    <div id="chartProcedencia"  style="margin-bottom:25px;float:left;height: 300px; width: 50%;"></div>
+    <div id="chartTransporte"  style="margin-bottom:25px;float:left;height: 300px; width: 50%;"></div>
+        <div id="chartSexo"  style="margin-bottom:25px;float:left;height: 300px; width: 50%;"></div>
+        <div id="chartAlojamiento"  style="margin-bottom:25px;float:left;height: 300px; width: 50%;"></div>
+        <div id="chartTipoAlojamiento"  style="margin-bottom:25px;float:left;height: 300px; width: 50%;"></div>
+        <div id="chartGastos"  style="margin-bottom:25px;float:left;height: 300px; width: 50%;"></div>
+        <div id="chartMotivo"  style="margin-bottom:25px;float:left;height: 300px; width: 50%;"></div>
+        <div id="chartInformo"  style="margin-bottom:25px;float:left;height: 300px; width: 50%;"></div>
+        <div id="chartViaje"  style="margin-bottom:25px;float:left;height: 300px; width: 50%;"></div>
+
+
+
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
 </div>
 <script>
 
 window.onload = function () {
- 
+
 var chartTransporte = new CanvasJS.Chart("chartTransporte", {
     animationEnabled: true,
     exportEnabled: true,
@@ -35,7 +35,7 @@ var chartTransporte = new CanvasJS.Chart("chartTransporte", {
         type: "column", //change type to bar, line, area, pie, etc
         // indexLabel: "{y}", //Shows y value on all Data Points
         indexLabelFontColor: "#5A5757",
-        // indexLabelPlacement: "outside",   
+        // indexLabelPlacement: "outside",
         dataPoints: {!! json_encode($transporte) !!}
     }]
 });
@@ -52,7 +52,7 @@ var chartProcedencia = new CanvasJS.Chart("chartProcedencia", {
         type: "column", //change type to bar, line, area, pie, etc
         // indexLabel: "{y}", //Shows y value on all Data Points
         indexLabelFontColor: "#5A5757",
-        // indexLabelPlacement: "outside",   
+        // indexLabelPlacement: "outside",
         dataPoints: {!! json_encode($procedencia) !!}
     }]
 });
@@ -70,26 +70,26 @@ var chartSexo = new CanvasJS.Chart("chartSexo", {
         type: "column", //change type to bar, line, area, pie, etc
         // indexLabel: "{y}", //Shows y value on all Data Points
         indexLabelFontColor: "#5A5757",
-        // indexLabelPlacement: "outside",   
+        // indexLabelPlacement: "outside",
         dataPoints: {!! json_encode($sexo) !!}
     }]
 });
 chartSexo.render();
 
-// 
+//
 
 var chartAlojamiento = new CanvasJS.Chart("chartAlojamiento", {
     animationEnabled: true,
     exportEnabled: true,
     theme: "light1", // "light1", "light2", "dark1", "dark2"
     title:{
-        text: "en que ciudad se aloja?"
+        text: "Ciudad donde se Aloja"
     },
     data: [{
         type: "column", //change type to bar, line, area, pie, etc
         // indexLabel: "{y}", //Shows y value on all Data Points
         indexLabelFontColor: "#5A5757",
-        // indexLabelPlacement: "outside",   
+        // indexLabelPlacement: "outside",
         dataPoints: {!! json_encode($lugar_alojamiento) !!}
     }]
 });
@@ -100,98 +100,98 @@ var chartTipoAlojamiento = new CanvasJS.Chart("chartTipoAlojamiento", {
     exportEnabled: true,
     theme: "light1", // "light1", "light2", "dark1", "dark2"
     title:{
-        text: "Donde se Aloja durante el evento?"
+        text: "Tipo de Alojamiento"
     },
     data: [{
         type: "column", //change type to bar, line, area, pie, etc
         // indexLabel: "{y}", //Shows y value on all Data Points
         indexLabelFontColor: "#5A5757",
-        // indexLabelPlacement: "outside",   
+        // indexLabelPlacement: "outside",
         dataPoints: {!! json_encode($tipoalojamiento) !!}
     }]
 });
 chartTipoAlojamiento.render();
 
-// 
+//
 
 var chartGastos = new CanvasJS.Chart("chartGastos", {
     animationEnabled: true,
     exportEnabled: true,
     theme: "light1", // "light1", "light2", "dark1", "dark2"
     title:{
-        text: "Cuanto estima que gastó durante la estadia por dia por persona?"
+        text: "Gastos durante la estadia (Por persona por dia)"
     },
     data: [{
         type: "column", //change type to bar, line, area, pie, etc
         // indexLabel: "{y}", //Shows y value on all Data Points
         indexLabelFontColor: "#5A5757",
-        // indexLabelPlacement: "outside",   
+        // indexLabelPlacement: "outside",
         dataPoints: {!! json_encode($gastos) !!}
     }]
 });
 chartGastos.render();
 
-// 
-// 
+//
+//
 
 var chartMotivo = new CanvasJS.Chart("chartMotivo", {
     animationEnabled: true,
     exportEnabled: true,
     theme: "light1", // "light1", "light2", "dark1", "dark2"
     title:{
-        text: "Motivo del Viaje?"
+        text: "Motivos del Viaje"
     },
     data: [{
         type: "column", //change type to bar, line, area, pie, etc
         // indexLabel: "{y}", //Shows y value on all Data Points
         indexLabelFontColor: "#5A5757",
-        // indexLabelPlacement: "outside",   
+        // indexLabelPlacement: "outside",
         dataPoints: {!! json_encode($motivo) !!}
     }]
 });
 chartMotivo.render();
 
-// 
-// 
+//
+//
 
 var chartInformo = new CanvasJS.Chart("chartInformo", {
     animationEnabled: true,
     exportEnabled: true,
     theme: "light1", // "light1", "light2", "dark1", "dark2"
     title:{
-        text: "Como se informo del evento?"
+        text: "¿Como se informo del evento?"
     },
     data: [{
         type: "column", //change type to bar, line, area, pie, etc
         // indexLabel: "{y}", //Shows y value on all Data Points
         indexLabelFontColor: "#5A5757",
-        // indexLabelPlacement: "outside",   
+        // indexLabelPlacement: "outside",
         dataPoints: {!! json_encode($informo) !!}
     }]
 });
 chartInformo.render();
 
-// 
-// 
+//
+//
 
 var chartViaje = new CanvasJS.Chart("chartViaje", {
     animationEnabled: true,
     exportEnabled: true,
     theme: "light1", // "light1", "light2", "dark1", "dark2"
     title:{
-        text: "Con quien vino de viaje?"
+        text: "¿Con quién vino de viaje?"
     },
     data: [{
         type: "column", //change type to bar, line, area, pie, etc
         // indexLabel: "{y}", //Shows y value on all Data Points
         indexLabelFontColor: "#5A5757",
-        // indexLabelPlacement: "outside",   
+        // indexLabelPlacement: "outside",
         dataPoints: {!! json_encode($viaje) !!}
     }]
 });
 chartViaje.render();
 
- 
+
 }
 </script>
 @endsection
